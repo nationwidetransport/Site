568065a8c10bb5c86dd7665c2d911ce62264ea28
johnson <johnson@bj.(none)>
2013-05-03 21:58:40 +0530

    Rails new app initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nationwide::Application.config.secret_token = 'e815eea8775f97a7b53addd7528645e31c5a7098ce77b2d8ce37bfa06190efc7eb59f61fd58d6680aa2a9632dca85f20695bfd1bcf795bf884bbcbc993b78986'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nationwide::Application.config.session_store :cookie_store, key: '_nationwide_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nationwide::Application.config.session_store :active_record_store
