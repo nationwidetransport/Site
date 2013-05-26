d736bc89a8368748aee19c7ca4c85faa754753c2
johnsonarokiaraj <johnsonarokiaraj@gmail.com>
2013-05-26 15:59:10 +0530

    Choose car page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Nationwide::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root :to => 'home#index'
+  match '/car' => 'home#car'
 
   match '/check' => 'home#check'
 
