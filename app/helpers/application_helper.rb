092df589b7e5025837c35ed504fbab8ed22c96c1
johnsonarokiaraj <johnsonarokiaraj@gmail.com>
2013-06-03 18:26:41 +0530

    Step1 UI completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def active(page, action1 = "", action2 = "") 
+    if action1 == "" && action2 == ""
+      controller.controller_name == page ? 'active' : nil
+    elsif(page && action1 && action2 == "")
+      controller.controller_name == page && controller.action_name == action1 ? 'active' : nil
+    else
+      controller.controller_name == page && controller.action_name == action1 || controller.action_name == action2 ? 'active' : nil
+    end
+  end
 end
